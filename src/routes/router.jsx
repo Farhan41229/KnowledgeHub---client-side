ac625e6613f34bd36437586785c14c6f4f076e9a
Farhan41229 <farhankhan@iut-dhaka.edu>
2025-06-16 22:51:14 +0600

    Register done

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router';
 import App from '../App';
 import HomeLayout from '../layouts/HomeLayout';
+import Register from '../pages/Register';
 const router = createBrowserRouter([
   {
     path: '/',
     element: <HomeLayout></HomeLayout>,
+  },
+  {
+    path: '/register',
+    element: <Register></Register>,
   },
 ]);
 
