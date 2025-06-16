2fe080dbc6aec6964d698054a89c67bb28c46517
Farhan41229 <farhankhan@iut-dhaka.edu>
2025-06-16 22:06:30 +0600

    Home Layout Done

---
+++ src/layouts/HomeLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeLayout = () => {
+  return (
+    <div>
+      <h1>This is Home Layout</h1>
+    </div>
+  );
+};
+
+export default HomeLayout;
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router';
 import App from '../App';
+import HomeLayout from '../layouts/HomeLayout';
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App></App>,
+    element: <HomeLayout></HomeLayout>,
   },
 ]);
 
