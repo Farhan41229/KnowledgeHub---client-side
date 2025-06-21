4a6ed3e04a39e20a16c8a5319b2482ecc8967559
Farhan41229 <farhankhan@iut-dhaka.edu>
2025-06-21 13:30:24 +0600

    Banner updated

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,10 +4,13 @@ import { Carousel } from 'react-responsive-carousel';
 import Banner1 from '../assets/Banner 1.png';
 import Banner2 from '../assets/Banner 2.png';
 import Banner3 from '../assets/Banner 3.png';
+import { Link } from 'react-router';
 const Banner = () => {
   return (
     <div>
-      <h1 className='text-center my-5 text-4xl lg:text-7xl font-bold'>Share Your Knowledege</h1>
+      <h1 className="text-center my-5 text-4xl lg:text-7xl font-bold">
+        Share Your Knowledege
+      </h1>
       <div className="divider"></div>
       <Carousel
         className="my-10"
@@ -28,6 +31,13 @@ const Banner = () => {
           <img src={Banner3} />
         </div>
       </Carousel>
+      <div className="divider"></div>
+      <div className="flex justify-center items-center">
+        <Link className="text-center text-2xl p-5 rounded-full bg-orange-500 lg:w-[500px] mx-auto hover:bg-orange-300 hover:cursor-pointer">
+          Explore Articles
+        </Link>
+      </div>
+      <div className="divider"></div>
     </div>
   );
 };
